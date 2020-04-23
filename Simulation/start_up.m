0cd7267d9126999d5fafc83fc8b1ca689d52718d
Yukai Gong <gyk199305@gmail.com>
2020-04-23 17:29:36 -0400

    Reorganize the folder

--- Simulation/start_up.m
+++ Simulation/start_up.m
@@ -1,1 +1,15 @@
+projectRoot = fileparts(mfilename('fullpath'));
+restoredefaultpath;
+% Construct paths to cache and code folders
+myCacheFolder = fullfile(projectRoot, 'Build', 'Cache');
+myCodeFolder = fullfile(projectRoot, 'Build', 'Code');
+
+% Set the file generation folder paths
+Simulink.fileGenControl('set',...
+    'CacheFolder', myCacheFolder,...
+    'CodeGenFolder', myCodeFolder,...
+    'createDir', true);
+root_dir = pwd;
+
+
 addpath(genpath(pwd));
