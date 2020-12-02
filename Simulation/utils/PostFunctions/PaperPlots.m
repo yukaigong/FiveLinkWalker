3a724af6bbaedf78648b4bc7ce683d8b02be81fc
gyk1993 <gyk199305@gmail.com>
2020-12-02 11:05:51 -0500

    update the ignore file

--- Simulation/utils/PostFunctions/PaperPlots.m
+++ Simulation/utils/PostFunctions/PaperPlots.m
@@ -1,19 +1,19 @@
 f1 = figure;
 ax1 = gca;
-set(gcf, 'Position',  [100, 100, 300, 200])
-plot(Data_perfect.l_stToe,'LineWidth',2)
+set(gcf, 'Position',  [100, 100, 300, 160])
+plot(Data.l_stToe,'LineWidth',2)
 hold on
 plot(100*Data.stanceLeg,'g-.','LineWidth',2)
 grid on
 title('')
 xlabel('\textbf{Time (s)}','Interpreter','latex')
 ylabel('{\boldmath$L^y$} \textbf{(kg-${\rm \textbf{m}}^{\boldmath2}$/s)}','Interpreter','latex')
-ylim([25,45])
+ylim([30,42])
 
 f2 = figure;
 ax2 = gca;
-set(gcf, 'Position',  [100, 100, 300, 200])
-plot(Data_perfect.lG,'LineWidth',2)
+set(gcf, 'Position',  [100, 100, 300, 160])
+plot(Data.lG,'LineWidth',2)
 hold on
 plot(100*Data.stanceLeg,'g-.','LineWidth',2)
 grid on
@@ -24,25 +24,26 @@ ylim([-10,20])
 
 f3 = figure;
 ax3 = gca;
-set(gcf, 'Position',  [100, 100, 300, 200])
-plot(Data_perfect.vx_com,'LineWidth',2)
+set(gcf, 'Position',  [100, 100, 300, 160])
+plot(Data.vx_com,'LineWidth',2)
 hold on
 plot(100*Data.stanceLeg,'g-.','LineWidth',2)
 grid on
 title('')
 xlabel('\textbf{Time (s)}','Interpreter','latex')
 ylabel('{\boldmath$v^x_{\rm CoM}$} \textbf{(m/s)}','Interpreter','latex')
-ylim([1,2.5])
+ylim([1.25,2.25])
 
 linkaxes([ax1,ax2,ax3],'x')
 xlim([14.8,15.5])
 % linkaxes([ax1,ax2],'y')
 
 % figure_path = [root_dir, '\PicsData\PD200820'];
-% cd(figure_path)
-% saveas(f1,'L_FLW.png')
-% saveas(f2,'LCOM_FLW.png')
-% saveas(f3,'vxCOM_FLW.png')
+figure_path = [root_dir, '\PicsData\PD201017_paper'];
+cd(figure_path)
+saveas(f1,'L_FLW.png')
+saveas(f2,'LCOM_FLW.png')
+saveas(f3,'vxCOM_FLW.png')
 
 %%
 
@@ -68,7 +69,7 @@ plot(Data.dx0_next,'LineWidth',2)
 plot(10*Data.stanceLeg,'g-.','LineWidth',2)
 hold off;
 grid on
-legend({'Instantaneous value of {\boldmath$\frac{L^y}{mH}$}','Predicted value at end of step'},'Interpreter','latex')
+% legend({'Instantaneous value of {\boldmath$\frac{L^y}{mH}$}','Predicted value at end of step'},'Interpreter','latex')
 xlabel('\textbf{Time (s)}','Interpreter','latex','FontSize',12)
 ylabel('{\boldmath$\frac{L^y}{mH}$} \textbf{(m/s)}','Interpreter','latex','FontSize',12)
 
@@ -82,7 +83,7 @@ plot(Data.vx0_next,'LineWidth',2)
 plot(10*Data.stanceLeg,'g-.','LineWidth',2)
 hold off;
 grid on
-legend({'Instantaneous value of {\boldmath$v^x_{\rm CoM}$}','Predicted value at end of step'},'Interpreter','latex')
+% legend({'Instantaneous value of {\boldmath$v^x_{\rm CoM}$}','Predicted value at end of step'},'Interpreter','latex')
 xlabel('\textbf{Time (s)}','Interpreter','latex')
 ylabel('{\boldmath$v^x_{\rm CoM}$} \textbf{(m/s)}','Interpreter','latex')
 
